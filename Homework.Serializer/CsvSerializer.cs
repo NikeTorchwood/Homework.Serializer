aaa8af73ed61dc30f9c9d2462d8aadceaca14dfb
ntwdconst@gmail.com <ntwdconst@gmail.com>
2024-09-04 23:00:36 +0700

    Добавьте файлы проекта.

---
+++ Homework.Serializer/CsvSerializer.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace Homework.Serializer;
+
+public class CsvSerializer
+{
+    public string Serialize<T>(T obj)
+    {
+        var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        var values = properties.Select(p => p.GetValue(obj)?.ToString() ?? string.Empty);
+        return string.Join(",", values);
+    }
+
+    public T Deserialize<T>(string csv) where T : new()
+    {
+        var obj = new T();
+        var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        var values = csv.Split(',');
+
+        for (var i = 0; i < properties.Length; i++)
+        {
+            if (i < values.Length)
+            {
+                properties[i].SetValue(obj, Convert.ChangeType(values[i], properties[i].PropertyType));
+            }
+        }
+
+        return obj;
+    }
+}
---
+++ Homework.Serializer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace Homework.Serializer;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var serializer = new CsvSerializer();
+        var fInstance = F.Get();
+
+        // Замер времени сериализации
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        for (var i = 0; i < 1000; i++)
+        {
+            var csv = serializer.Serialize(fInstance);
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine($"Время на сериализацию = {stopwatch.ElapsedMilliseconds} мс");
+
+        // Замер времени десериализации
+        var csvData = serializer.Serialize(fInstance);
+        stopwatch.Restart();
+
+        for (var i = 0; i < 1000; i++)
+        {
+            var deserializedInstance = serializer.Deserialize<F>(csvData);
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine($"Время на десериализацию = {stopwatch.ElapsedMilliseconds} мс");
+
+        stopwatch.Restart();
+        for (var i = 0; i < 1000; i++)
+        {
+            var json = JsonConvert.SerializeObject(fInstance);
+        }
+        stopwatch.Stop();
+        Console.WriteLine($"Стандартный механизм (Newtonsoft.Json) - Время на сериализацию = {stopwatch.ElapsedMilliseconds} мс");
+
+        // Замер времени десериализации с использованием Newtonsoft.Json
+        var jsonData = JsonConvert.SerializeObject(fInstance);
+        stopwatch.Restart();
+        for (var i = 0; i < 1000; i++)
+        {
+            var deserializedInstance = JsonConvert.DeserializeObject<F>(jsonData);
+        }
+        stopwatch.Stop();
+        Console.WriteLine($"Стандартный механизм (Newtonsoft.Json) - Время на десериализацию = {stopwatch.ElapsedMilliseconds} мс");
+    }
+}
